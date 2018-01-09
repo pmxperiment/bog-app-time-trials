d3fab8ca195ea2abd6872b7f09158a7ef7e8b251
GA LOANER 2 <lenstesterguy@gmail.com>
2018-01-08 17:04:48 -0800

    to repo

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root "creatures#index" # localhost:3000
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/creatures", to: "creatures#index" #localhost
+  get "/creatures", to: "creatures#index", as: "creatures" #localhost
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
 end
